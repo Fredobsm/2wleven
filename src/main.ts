d605772959865009a6e419e7c19203e7b066e8c7
Fredobsm <72887227+Fredobsm@users.noreply.github.com>
2023-02-11 21:15:29 -0500

    1 file modified, 2 added

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,84 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
+  selector: 'app-product-list',
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <div>
+      <input [(ngModel)]="sortBy" placeholder="Sort By">
+      <input [(ngModel)]="filterCriteria" placeholder="Filter">
+    </div>
+    <div *ngFor="let product of products | sort:sortBy | filter:filterCriteria">
+      <h2>
+        <a [routerLink]="['/product', product.id]">
+          {{ product.name }}
+        </a>
+      </h2>
+      <p>{{ product.description }}</p>
+    </div>
   `,
+  styleUrls: [import { Component, OnInit } from '@angular/core';
+  import { Product } from '../product';
+  import { ProductService } from '../product.service';
+  
+  @Component({
+    selector: 'app-product-list',
+    template: `
+      <div>
+        <input [(ngModel)]="sortBy" placeholder="Sort By">
+        <input [(ngModel)]="filterCriteria" placeholder="Filter">
+      </div>
+      <div *ngFor="let product of products | sort:sortBy | filter:filterCriteria">
+        <h2>
+          <a [routerLink]="['/product', product.id]">
+            {{ product.name }}
+          </a>
+        </h2>
+        <p>{{ product.description }}</p>
+      </div>
+    `,
+    styleUrls: ['./product-list.component.css']
+  })
+  export class ProductListComponent implements OnInit {
+    products: Product[];
+    sortBy: string;
+    filterCriteria: string;
+  
+    constructor(private productService: ProductService) { }
+  
+    ngOnInit() {
+      this.getProducts();
+    }
+  
+    getProducts(): void {
+      this.productService.getProducts()
+        .subscribe(products => this.products = products,
+          error => {
+            console.error(error);
+            this.products = [];
+          });
+    }
+  }
+  ]
 })
-export class App {
-  name = 'Angular';
-}
+export class ProductListComponent implements OnInit {
+  products: Product[];
+  sortBy: string;
+  filterCriteria: string;
 
-bootstrapApplication(App);
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts()
+      .subscribe(products => this.products = products,
+        error => {
+          console.error(error);
+          this.products = [];
+        });
+  }
+}
