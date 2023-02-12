ffd96647f43e02129055e74d4ad8aa1b0fe17bce
Fredobsm <72887227+Fredobsm@users.noreply.github.com>
2023-02-11 22:25:29 -0500

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Shop2ElevenVerified.com</title>
+  <title>Shop2elevenverified.com</title>
   <base href="/">
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
+
   <!-- Add CSS files for the Angular components -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <app-root></app-root>
+
   <!-- Add JavaScript files for the Angular components -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.16.0/dist/umd/popper.min.js" integrity="sha384-7yb5tpjU5J9UQ+HUHL7ZM1bQRfO+nt4N/yVgAF/NPrq+pN/N/OkmZdbLNz+jND7V" crossorigin="anonymous"></script>
@@ -25,5 +28,4 @@
   <script src="main.js"></script>
 </body>
 </html>
-
 
